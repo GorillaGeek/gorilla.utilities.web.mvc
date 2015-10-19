ad8c3d3a8e70b3547f545209f4dc7767eeb62d85
Daniel Prado <danieloprado@outlook.com>
2015-10-19 18:31:49 +0100

    simplify template for

--- src/HtmlExtensions/TemplateFor.cs
+++ src/HtmlExtensions/TemplateFor.cs
@@ -9,89 +9,130 @@ namespace System.Web.Mvc.Html
     /// <summary>
     /// Create a input for Angular ngRepeat
     /// </summary>
-    internal class TemplateForExtensions
+    public static class TemplateForExtensions
     {
-        /// <summary>
-        /// Create a input for Angular ngRepeat
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TValue"></typeparam>
-        /// <param name="htmlHelper"></param>
-        /// <param name="expression">Lambda for model</param>
-        /// <param name="htmlAttributes">Additional html attrs</param>
-        /// <param name="inputType">Type of input</param>
-        /// <param name="numberReplace">number of replacements</param>
-        /// <param name="value">Current value for input</param>
-        /// <returns></returns>
-        public static MvcHtmlString TemplateFor<TModel, TValue>(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, IDictionary<string, object> htmlAttributes, string inputType, int? numberReplace = null, string value = "")
+
+
+        public static MvcHtmlString DropDownListTemplateFor<TModel, TEnum>(
+           this HtmlHelper<TModel> htmlHelper,
+           Expression<Func<TModel, TEnum>> expression,
+           IEnumerable<SelectListItem> selectList,
+           string optionLabel = null,
+           IDictionary<string, object> htmlAttributes = null)
         {
-            var metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            var result = htmlHelper.DropDownListFor(expression, selectList, optionLabel, htmlAttributes);
+            var stringResult = TemplateForExtensions.ReplaceNameAndId(result.ToString(), htmlHelper, expression, null);
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+        public static MvcHtmlString EnumDropDownListTemplateFor<TModel, TEnum>(
+           this HtmlHelper<TModel> htmlHelper,
+           Expression<Func<TModel, TEnum>> expression,
+           string optionLabel = null,
+           IDictionary<string, object> htmlAttributes = null)
+        {
+            var result = htmlHelper.EnumDropDownListFor(expression, optionLabel, htmlAttributes);
+            var stringResult = TemplateForExtensions.ReplaceNameAndId(result.ToString(), htmlHelper, expression, null);
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+        public static MvcHtmlString HiddenTemplateFor<TModel, TValue>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TValue>> expression,
+            object htmlAttributes = null,
+            int? numberReplace = null)
+        {
+            var result = htmlHelper.HiddenFor(expression, htmlAttributes);
+            var stringResult = ReplaceNameAndId(result.ToString(), htmlHelper, expression, numberReplace);
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+        public static MvcHtmlString CheckBoxTemplateFor<TModel, TValue>(
+           this HtmlHelper<TModel> htmlHelper,
+           Expression<Func<TModel, bool>> expression,
+           object htmlAttributes = null,
+           int? numberReplace = null)
+        {
+            var result = htmlHelper.CheckBoxFor(expression, htmlAttributes);
+            var stringResult = ReplaceNameAndId(result.ToString(), htmlHelper, expression, numberReplace);
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+        public static MvcHtmlString RadioButtonTemplateFor<TModel, TValue>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TValue>> expression,
+            object value,
+            object htmlAttributes = null,
+            int? numberReplace = null)
+        {
+            var result = htmlHelper.RadioButtonFor(expression, value, htmlAttributes);
+            var stringResult = ReplaceNameAndId(result.ToString(), htmlHelper, expression, numberReplace);
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+        public static MvcHtmlString TextAreaTemplateFor<TModel, TValue>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TValue>> expression,
+            object htmlAttributes = null,
+            int? numberReplace = null)
+        {
+            var result = htmlHelper.TextAreaFor(expression, htmlAttributes);
+            var stringResult = ReplaceNameAndId(result.ToString(), htmlHelper, expression, numberReplace);
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+
+        public static MvcHtmlString TextBoxTemplateFor<TModel, TValue>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TValue>> expression,
+            object htmlAttributes = null,
+            int? numberReplace = null)
+        {
+            var result = htmlHelper.TextBoxFor(expression, htmlAttributes);
+            var stringResult = ReplaceNameAndId(result.ToString(), htmlHelper, expression, numberReplace);
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+        public static MvcHtmlString ValidationMessageTemplateFor<TModel, TValue>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TValue>> expression,
+            string validationMessage = null,
+            object htmlAttributes = null,
+            string tag = "span",
+            int? numberReplace = null)
+        {
+            var result = htmlHelper.ValidationMessageFor(expression, validationMessage, htmlAttributes, tag);
+
+            var nameReg = new Regex("\\sdata-valmsg-for=\"[^\"]+\"");
+            var name = GenerateName(htmlHelper, expression, numberReplace);
+            var stringResult = nameReg.Replace(result.ToString(), $" data-valmsg-for=\"{name}\"");
+
+            return MvcHtmlString.Create(stringResult);
+        }
+
+        private static string ReplaceNameAndId(string stringResult, HtmlHelper htmlHelper, LambdaExpression expression, int? numberReplace)
+        {
+            var nameReg = new Regex("\\sname=\"[^\"]+\"");
+            var name = GenerateName(htmlHelper, expression, numberReplace);
+            stringResult = nameReg.Replace(stringResult, $" name=\"{name}\"");
+
+            var idReg = new Regex("\\sid=\"[^\"]+\"");
+            var id = GenerateId(name);
+            stringResult = idReg.Replace(stringResult, $"id=\"{id}\"");
+
+            return stringResult;
+        }
+
+        private static string GenerateName(HtmlHelper htmlHelper, LambdaExpression expression, int? numberReplace)
+        {
             var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
-            var fullName = GenerateName(htmlHelper, expression, numberReplace);
-            var tagBuilder = inputType == "textarea" ? TemplateBaseForTextArea(value) : TemplateBaseForInput(inputType, value);
-
-            tagBuilder.MergeAttribute("id", GenerateId(fullName));
-            tagBuilder.MergeAttribute("name", fullName, true);
-
-            tagBuilder.MergeAttributes(htmlHelper.GetUnobtrusiveValidationAttributes(htmlFieldName, metadata));
-
-            if (htmlAttributes == null)
-            {
-                return new MvcHtmlString(tagBuilder.ToString(TagRenderMode.Normal));
-            }
-
-            foreach (var attrs in htmlAttributes)
-            {
-                tagBuilder.MergeAttribute(attrs.Key.Replace("_", "-"), attrs.Value.ToString(), true);
-            }
-
-            return new MvcHtmlString(tagBuilder.ToString(TagRenderMode.Normal));
-        }
-
-        private static TagBuilder TemplateBaseForInput(string inputType, string value)
-        {
-            var tagBuilder = new TagBuilder("input");
-            tagBuilder.MergeAttribute("type", inputType);
-            tagBuilder.MergeAttribute("value", value);
-
-            return tagBuilder;
-        }
-
-
-        private static TagBuilder TemplateBaseForTextArea(string value)
-        {
-            var tagBuilder = new TagBuilder("textarea")
-            {
-                InnerHtml = value
-            };
-
-            return tagBuilder;
-        }
-
-
-        /// <summary>
-        /// Generate a name for input
-        /// </summary>
-        /// <param name="htmlHelper"></param>
-        /// <param name="expression"></param>
-        /// <param name="numberReplace"></param>
-        /// <returns></returns>
-        public static string GenerateName(HtmlHelper htmlHelper, LambdaExpression expression, int? numberReplace)
-        {
-            var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
-            return GenerateName(htmlHelper, htmlFieldName, numberReplace);
-        }
-
-        /// <summary>
-        /// Generate a name for input
-        /// </summary>
-        /// <param name="htmlHelper"></param>
-        /// <param name="htmlFieldName"></param>
-        /// <param name="numberReplace"></param>
-        /// <returns></returns>
-        private static string GenerateName(HtmlHelper htmlHelper, string htmlFieldName, int? numberReplace)
-        {
-
             var fullName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(htmlFieldName);
 
             var find = new Regex(@"\[\d+\]");
@@ -100,8 +141,8 @@ namespace System.Web.Mvc.Html
             var z = 0;
             while (find.Matches(fullName).Count > 0)
             {
-                fullName = fullName.ReplaceRegLastOccurrence(@"\[\d+\]", string.Format($"[{TemplateSettings.Pattern}]", replace));
-                replace = string.Format(TemplateSettings.ParentIndex, replace);
+                fullName = fullName.ReplaceRegLastOccurrence(@"\[\d+\]", String.Format($"[{TemplateSettings.Pattern}]", replace));
+                replace = String.Format(TemplateSettings.ParentIndex, replace);
 
                 z++;
                 if (z == 100 || (numberReplace != null && z == numberReplace))
@@ -111,33 +152,11 @@ namespace System.Web.Mvc.Html
             return fullName;
         }
 
-        /// <summary>
-        /// Generate the id based on name
-        /// </summary>
-        /// <param name="fullName"></param>
-        /// <returns></returns>
         private static string GenerateId(string fullName)
         {
             return fullName.Replace("[", "_").Replace("]", "_").Replace(".", "_").Replace("_$", ".$");
         }
 
-        internal static string ReplaceNameAndId(string stringResult, HtmlHelper htmlHelper, LambdaExpression expression, int? numberReplace)
-        {
-            var htmlFieldName = ExpressionHelper.GetExpressionText(expression);
-            return ReplaceNameAndId(stringResult, htmlHelper, htmlFieldName, numberReplace);
-        }
 
-        internal static string ReplaceNameAndId(string stringResult, HtmlHelper htmlHelper, string htmlFieldName, int? numberReplace)
-        {
-            var nameReg = new Regex("\\sname=\"[^ \"]+\"");
-            var name = GenerateName(htmlHelper, htmlFieldName, numberReplace);
-            stringResult = nameReg.Replace(stringResult, $" name=\"{name}\"");
-
-            var idReg = new Regex("\\sid=\"[^ \"]+\"");
-            var id = GenerateId(name);
-            stringResult = idReg.Replace(stringResult, $"id=\"{id}\"");
-
-            return stringResult;
-        }
     }
 }
